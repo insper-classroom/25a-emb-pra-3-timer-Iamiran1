d8699606d73a3c951fb92df068b5af7090d541ec
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-03-13 12:36:06 -0300

    Starting coding

--- main/main.c
+++ main/main.c
@@ -1,17 +1,98 @@
 /**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ * Copyright (c) 2021 Raspberry Pi (Trading) Ltd.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/gpio.h"
+ #include <stdio.h>
+ #include "hardware/rtc.h"
+ #include "pico/stdlib.h"
+ #include "pico/util/datetime.h"
+ 
+ static volatile bool fired = false;
+ 
+ static void alarm_callback(void) {
+     fired = true;
+ }
 
-int main() {
-    stdio_init_all();
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+ const int ECHO_PIN = 6;
+ const int TRIG_PIN = 7;
+
+ bool echo_got = false;
+ void gpio_callback(uint gpio, uint32_t events)
+{
+    if (gpio_get(ECHO_PIN) == 1){
+        echo_got = true;
+    }
+    { // fall edge
+       
     }
 }
+
+void send_trig_pulse(){
+    gpio_put(TRIG_PIN,1);
+    sleep_ms(5);
+    gpio_put(TRIG_PIN,0);
+}
+ int main() {
+     stdio_init_all();
+     printf("RTC Alarm Repeat!\n");
+     gpio_init(ECHO_PIN);
+     gpio_set_dir(ECHO_PIN, GPIO_IN);
+     gpio_set_irq_enabled_with_callback(ECHO_PIN,
+         GPIO_IRQ_EDGE_RISE |
+         GPIO_IRQ_EDGE_FALL,
+         true,
+         &gpio_callback);
+ 
+     gpio_init(TRIG_PIN);
+     gpio_set_dir(TRIG_PIN, GPIO_OUT);
+     
+     // Start on Wednesday 13th January 2021 11:20:00
+     datetime_t t = {
+         .year  = 2020,
+         .month = 01,
+         .day   = 13,
+         .dotw  = 3, // 0 is Sunday, so 3 is Wednesday
+         .hour  = 11,
+         .min   = 20,
+         .sec   = 00
+     };
+ 
+     // Start the RTC
+     rtc_init();
+     rtc_set_datetime(&t);
+ 
+     // Alarm once a minute
+     datetime_t alarm = {
+         .year  = -1,
+         .month = -1,
+         .day   = -1,
+         .dotw  = -1,
+         .hour  = -1,
+         .min   = -1,
+         .sec   = 00
+     };
+ 
+     rtc_set_alarm(&alarm, &alarm_callback);
+ 
+     // Alarm will keep firing forever
+     while(1){
+        send_trig_pulse();
+        sleep_ms(10);
+        if(echo_got){
+            printf("Foi");
+            echo_got = false;
+        }
+         if (fired) {
+             fired = 0;
+             datetime_t t = {0};
+             rtc_get_datetime(&t);
+             char datetime_buf[256];
+             char *datetime_str = &datetime_buf[0];
+             datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+             printf("Alarm Fired At %s\n", datetime_str);
+         }
+ 
+     }
+ }

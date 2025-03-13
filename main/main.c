0568daae9895d966fb784e192793e59032440936
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-03-13 19:39:54 -0300

    quase finalizada

--- main/main.c
+++ main/main.c
@@ -19,22 +19,28 @@
  const int TRIG_PIN = 7;
 
  bool echo_got = false;
+ volatile uint32_t start_us;
+ volatile uint32_t end_us;
+
  void gpio_callback(uint gpio, uint32_t events)
 {
     if (gpio_get(ECHO_PIN) == 1){
         echo_got = true;
+        start_us =  get_absolute_time();
+        end_us = 0;
     }
-    { // fall edge
-       
+    else if(gpio_get(ECHO_PIN)==0){
+        end_us = get_absolute_time();
     }
 }
 
-void send_trig_pulse(){
+void send_trig_pulse(){   
     gpio_put(TRIG_PIN,1);
     sleep_ms(5);
     gpio_put(TRIG_PIN,0);
 }
  int main() {
+    sleep_ms(1000);
      stdio_init_all();
      printf("RTC Alarm Repeat!\n");
      gpio_init(ECHO_PIN);
@@ -73,17 +79,11 @@ void send_trig_pulse(){
          .min   = -1,
          .sec   = 00
      };
- 
      rtc_set_alarm(&alarm, &alarm_callback);
- 
      // Alarm will keep firing forever
      while(1){
         send_trig_pulse();
-        sleep_ms(10);
-        if(echo_got){
-            printf("Foi");
-            echo_got = false;
-        }
+        rtc_enable_alarm();
          if (fired) {
              fired = 0;
              datetime_t t = {0};
@@ -91,7 +91,25 @@ void send_trig_pulse(){
              char datetime_buf[256];
              char *datetime_str = &datetime_buf[0];
              datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
-             printf("Alarm Fired At %s\n", datetime_str);
+             printf("%s - Falha \n",datetime_str);
+            }
+         if(!fired){
+            if(echo_got){
+                rtc_disable_alarm();
+                uint32_t delta_t;
+                while(end_us == 0  ){
+                    delta_t = end_us - start_us;
+                }
+                delta_t = end_us - start_us;
+                float distancia = (float) delta_t * 0.017015;
+                datetime_t t = {0};
+                rtc_get_datetime(&t);
+                char datetime_buf[256];
+                char *datetime_str = &datetime_buf[0];
+                datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+                printf("%s - %f cm \n",datetime_str,distancia);
+                echo_got = false;
+            }
          }
  
      }
